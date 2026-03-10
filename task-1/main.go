6d4c4180bd82475714da956e6efd8cdfca2c6f07
Maksim Torzhkov <maxtorj@yandex.ru>
2026-03-10 20:41:48 +0300

    add main

---
+++ task-1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"kafka-broker-demo/task-1/kafka/consumer"
+	"kafka-broker-demo/task-1/kafka/producer"
+	"log"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+const (
+	brokerAddr = "localhost:9092"
+	topicName = "test-topic"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_6_0_0
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+
+	admin, err := sarama.NewClusterAdmin([]string{brokerAddr}, config)
+	if err != nil {
+		log.Fatalf("Can't create ClusterAdmin: %v", err)
+	}
+
+	defer func() { _ = admin.Close() }()
+
+	if err = createTopic(admin, topicName); err != nil {
+		log.Fatalf("Topic creation error: %v", err)
+	}
+	log.Printf("Topic '%s' success created or already exists.", topicName)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		if err := producer.RunProducer(brokerAddr, topicName, config); err != nil {
+			log.Printf("Producer error: %v", err)
+		}
+	}()
+
+	if err := consumer.RunConsumer(brokerAddr, topicName, config); err != nil {
+		log.Fatalf("Consumer error: %v", err)
+	}
+	log.Println("App complete")
+}
+
+func createTopic(admin sarama.ClusterAdmin, topic string) error {
+	topicDetail := &sarama.TopicDetail{
+		NumPartitions: 1,
+		ReplicationFactor: 1,
+	}
+	return admin.CreateTopic(topic, topicDetail, false)
+}
