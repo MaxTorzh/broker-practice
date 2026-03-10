4d8a0313170bda617b103c65cf761d5e51860822
Maksim Torzhkov <maxtorj@yandex.ru>
2026-03-10 19:56:04 +0300

    add producer for the task-1

---
+++ task-1/producer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+func RunProducer(brokers string, topic string, baseConfig *sarama.Config) error {
+
+	producer, err := sarama.NewSyncProducer([]string{brokers}, baseConfig)
+	if err != nil {
+		return fmt.Errorf("Can't create a producer: %w", err)
+	}
+
+	defer func() {_ = producer.Close()}() // закрытие producer для освобождения ресурсов
+
+	fmt.Println("Producer starts, sending messages...")
+
+	for i := range 10 {
+		message := fmt.Sprintf("Hello, Kafka! Message #%d", i)
+		msg := &sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.StringEncoder(message),
+		}
+
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			return fmt.Errorf("Sending message error %d: %w", i, err)
+		}
+
+		fmt.Printf("Message %d send to: partition %d, offset %d\n", i, partition, offset)
+	}
+
+	fmt.Println("Producer finished work")
+	return nil
+}
